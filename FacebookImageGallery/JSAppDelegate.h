16966d6772580f2dccf8714a7ac703fb37532c5d
Jack Sisson <jack@aurnh.com>
2012-01-19 23:12:37 -0500

    Initial Commit

---
+++ FacebookImageGallery/JSAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  JSAppDelegate.h
+//  FacebookImageGallery
+//
+//  Created by Jack Sisson on 1/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class JSViewController;
+
+@interface JSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) JSViewController *viewController;
+
+@end
---
+++ FacebookImageGallery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FacebookImageGallery
+//
+//  Created by Jack Sisson on 1/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JSAppDelegate class]));
+    }
+}
